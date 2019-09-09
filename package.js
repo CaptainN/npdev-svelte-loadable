ad9cb36af0a5906affc020b22fc47a2c5ce22d54
Kevin Newman <Kevin@unFocus.com>
2019-09-09 00:49:54 -0400

    svelte:compiler needs a versoin constraint to publish

--- package.js
+++ package.js
@@ -7,7 +7,7 @@ Package.describe({
 
 Package.onUse(function (api) {
   api.versionsFrom('METEOR@1.5')
-  api.use(['ecmascript', 'ejson', 'svelte:compiler'])
+  api.use(['ecmascript', 'ejson', 'svelte:compiler@3.6.7_1'])
 
   api.mainModule('svelte-loadable-server.js', 'server', { lazy: true })
   api.mainModule('svelte-loadable-client.js', 'client', { lazy: true })
