1ee8dbdb5e4562d65002bcf34ebb37d66fb493ae
Kevin Newman <kevin@unfocus.com>
2020-04-22 00:49:03 -0400

    Increase versonsFrom and bump release

--- package.js
+++ package.js
@@ -1,12 +1,12 @@
 Package.describe({
   name: "npdev:svelte-loadable",
-  version: "1.0.0-alpha.1",
+  version: "1.0.0-alpha.2",
   summary: "Easy code splitting with Svelte-loadable",
   git: "https://github.com/CaptainN/npdev-svelte-loadable.git"
 })
 
 Package.onUse(function (api) {
-  api.versionsFrom('METEOR@1.5')
+  api.versionsFrom(['METEOR@1.5', 'METEOR@1.10'])
   api.use(['ecmascript', 'ejson', 'svelte:compiler@3.6.7_1'])
 
   api.mainModule('svelte-loadable-server.js', 'server', { lazy: true })
